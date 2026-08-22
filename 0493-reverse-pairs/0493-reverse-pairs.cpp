338a5ed452c05bdceb45cdbb83554954552beaa3
Parth Jadhav <jparth.2007@gmail.com>
2026-08-22 21:03:40 +0530

    Time: 620 ms (5.05%), Space: 53 MB (95%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int reversePairs(vector<int>& nums) {
+        int n = nums.size();
         vector<int> sorted;
         int cnt = 0;
 
-        for (int j = 0; j < nums.size(); j++) {
+        for (int j = 0; j < n; j++) {
             long long target = 2LL * nums[j] + 1;
             auto it = lower_bound(sorted.begin(), sorted.end(), target);
             cnt += (sorted.end() - it);
