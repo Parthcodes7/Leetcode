52cafbfed13c19308d128282b71074e90700e0b1
Parth Jadhav <jparth.2007@gmail.com>
2026-08-30 11:46:05 +0530

    Time: 16 ms (19.42%), Space: 35.1 MB (54.25%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int findDays(vector<int> &weights, int cap){
-        int n = weights.size();
         int days = 1, load = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < weights.size(); i++){
             if(weights[i] + load > cap){
                 days += 1;
                 load = weights[i];
