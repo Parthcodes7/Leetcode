ada0bdf2da94d22d75ec597a7fb941af59b7605e
Parth Jadhav <jparth.2007@gmail.com>
2026-09-14 14:44:47 +0530

    Time: 79 ms (5.05%), Space: 86.3 MB (5.01%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    ListNode* findMiddle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+        // Check fast->next->next to pick the FIRST middle node for even lengths
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+        while(list1 != nullptr && list2 != nullptr){
+            if(list1->val < list2->val){
+                temp->next = list1;
+                temp = list1;
+                list1 = list1->next;
+            }
+            else{
+                temp->next = list2;
+                temp = list2;
+                list2 = list2->next;
+            }
+        }
+        if(list1) temp->next = list1;
+        else temp->next = list2;
+
+        ListNode* res = dummyNode->next;
+        delete dummyNode; // Free memory
+        return res;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+
+        ListNode* middle = findMiddle(head);
+        ListNode* right = middle->next;
+        middle->next = nullptr;
+        ListNode* left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+        return mergeTwoLists(left, right);
+    }
+};
